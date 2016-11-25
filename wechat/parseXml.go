1c825f8bef0d24d0aa42c0a8748ee5ebd59d8548
Wang Cong <ousatoru@hotmail.com>
2016-11-25 09:59:55 +0800

    Msg whole

--- wechat/parseXml.go
+++ wechat/parseXml.go
@@ -9,11 +9,32 @@ import (
 	"time"
 )
 
+// Get msgtype of xml
+func MsgType(r *http.Request) string {
+	resp, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Println(err)
+		return "Invalid-err"
+	}
+	body := &Typ{}
+	xml.Unmarshal(resp, body)
+	fmt.Println("Msgtype: ", body.MsgType)
+	return body.MsgType
+	/*log.Printf("%q\n", resp)
+	reg := regexp.MustCompile(`<MsgType>(.*?)</MsgType>`)
+	typ := reg.FindStringSubmatch(string(resp))
+	if len(typ) > 0 {
+		return typ[1]
+	} else {
+		return "Invalid-blank"
+	}*/
+}
+
 // Parse args from the message that post from wechat, so r REQUEST.
 func PsText(r *http.Request) *TextMsg {
 	resp, err := ioutil.ReadAll(r.Body)
 	if err != nil {
-		log.Fatal(err)
+		log.Println(err)
 		return nil
 	}
 	body := &TextMsg{}
@@ -31,6 +52,28 @@ func MkText(fromUserName, toUserName, content string) ([]byte, error) {
 	textReply.MsgType = "text"
 	textReply.CreateTime = time.Duration(time.Now().Unix())
 	return xml.MarshalIndent(textReply, " ", "  ")
+}
+
+type BigMsg struct {
+	XMLName      xml.Name `xml:"xml"`
+	ToUserName   string
+	FromUserName string
+	CreateTime   time.Duration
+	MsgType      string
+	Content      string
+	PictureUrl   string
+	MediaId      string
+	Format       string
+	Recognition  string
+	ThumbMediaId string
+	Location_X   string
+	Location_Y   string
+	Scale        string
+	Label        string
+	Title        string
+	Description  string
+	Url          string
+	MsgId        int64
 }
 
 type Common struct {
@@ -84,6 +127,7 @@ type PositionMsg struct {
 }
 
 type LinkMsg struct {
+	Common
 	Title       string
 	Description string
 	Url         string
